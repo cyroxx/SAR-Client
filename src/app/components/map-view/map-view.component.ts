0099078284d528bb57cf636aadeeda6b95b8a636
niczem <nic@transparency-everywhere.com>
2017-01-20 23:09:47 +0100

    map service added. initmap added with osm

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import {MapService} from 'app/services/map.service'
 
+
+declare var L: any;
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
-  styleUrls: ['./map-view.component.css']
+  styleUrls: ['./map-view.component.css'],
+  providers:[MapService]
 })
 export class MapViewComponent implements OnInit {
+	public map: any;
+	mapService;
+  	constructor(MapService:MapService) {
+  		this.mapService = MapService;
+  	}
 
-  constructor() { }
+  	ngOnInit() {
+  		this.initMap();	
+  	}
 
-  ngOnInit() {
-  }
+  	initMap(){
 
+  		this.mapService.initMap()
+	}
 }
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+declare var L: any;
+@Injectable()
+export class MapService {
+  	map;
+  	startLocation;
+  	mapContainerId;
+  	maptype;
+  	constructor() { 
+  		this.maptype = 'OSM'
+  	}
+
+  	getMapObject() {
+  		this.initMap();
+  		return this.map;
+  	}
+
+
+  	initMap() {
+  		//init leaflet
+  		this.mapContainerId = 'client_map_container';
+
+      
+        this.startLocation = [32.46,16.87]
+        this.maptype = 'osm'
+
+        this.map = L.map(this.mapContainerId, '')
+        this.map.options.maxZoom = 10;
+        this.map.setView(this.startLocation, 7);;
+        var self = this;
+        switch(this.maptype){
+            case'offline-map':
+            	/*Not working until maptiles are added to the repo*/
+                L.tileLayer('MapQuest/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://sea-watch.org">Sea-Watch</a>'
+                }).addTo(this.map);
+           
+            break;
+            case'osm':
+
+            	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+						maxZoom: 19,
+						attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+					}).addTo(this.map);
+            break;
+	    }
+      return this.map;
+
+  	}
+}
