6a27e1d1b65bd59dc883f31921f0c8f866a10e2b
niczem <nic@transparency-everywhere.com>
2017-01-25 23:55:54 +0100

    auth modified

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
-
+declare var PouchDB: any;
+declare var localStorage: any;
 @Injectable()
 export class AuthService {
+  logged_in;
+  constructor() {
+  	this.logged_in = false;
 
-  constructor() { }
+  	if(localStorage.username != null && localStorage.password != null){
+  		//do login
+  	}else{
+
+  	}
+  }
+  is_logged_in() {
+  	return this.logged_in;
+  }
 
 }
