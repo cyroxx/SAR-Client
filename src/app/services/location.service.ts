42d238444e852a5ef1ed7295b0ef50b9a7f94ceb
Matthias Jacob <dev@matthiasjacob.net>
2017-02-12 21:09:19 +0100

    added bancroft example

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Bancroft } from 'bancroft';
 
 @Injectable()
 export class LocationService {
 
-  constructor() { }
+  private tracker: Bancroft;
+
+  constructor() {
+    this.tracker = new Bancroft();
+    this.tracker.on('connect', function () {
+      console.log('connected');
+    });
+    this.tracker.on('location', function (location) {
+      console.log('got new location');
+    });
+    this.tracker.on('satellite', function (satellite) {
+      console.log('got new satellite state');
+    });
+    this.tracker.on('disconnect', function (err) {
+      console.log('disconnected');
+    });
+  }
 
   getCurrentPosition(callback: (object) => void) {
     callback({coords: {latitude: 52.537, longitude: 13.253}, timestamp: 1486924248002});
