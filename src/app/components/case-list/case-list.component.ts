a89ba8cb1b0745416efd42caaf8a1f6386fae489
Hendrik Simon <simon@koop-bremen.de>
2017-02-22 19:48:54 +0100

    Improvement of case-list

--- src/app/components/case-list/case-list.component.ts
+++ src/app/components/case-list/case-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Case} from '../../interfaces/case';
+import {Case, Status, BoatType, BoatCondition} from '../../interfaces/case';
 import {CasesService} from '../../services/cases.service';
 import { ModalContainer, Modal } from '../../interfaces/modalcontainer';
 
@@ -14,8 +14,12 @@ import { ModalContainer, Modal } from '../../interfaces/modalcontainer';
 export class CaseListComponent implements OnInit {
 
   cases: Array<Case>;
+  states = Status;
+  boatTypes = BoatType;
+  boatConditions = BoatCondition;
 
-  constructor(private caseService: CasesService) { }
+  constructor(private caseService: CasesService) {
+   }
 
   ngOnInit() {
 
@@ -24,5 +28,22 @@ export class CaseListComponent implements OnInit {
     });
 
   }
+
+  getStateName(state: number) : string{
+    return this.states[state];
+  }
+
+  getStateClassName(state: number) : string{
+    return this.getStateName(state).replace(/ /g,'').toLowerCase();
+  }
+
+  getBoatTypeName(type: number) : string{
+    return this.boatTypes[type];
+  }
+
+  getBoatConditionName(condition: number) : string{
+    return this.boatConditions[condition];
+  }
+
 
 }
