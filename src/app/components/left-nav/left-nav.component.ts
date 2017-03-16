99b30d25626c280d09fd5129cb6da4462994af00
Hauke Lübbers <dubbel14@googlemail.com>
2017-03-16 19:53:37 +0100

    show cases/vehicles at location on map

--- src/app/components/left-nav/left-nav.component.ts
+++ src/app/components/left-nav/left-nav.component.ts
@@ -2,33 +2,40 @@ import { Component, OnInit } from '@angular/core';
 
 import { VehiclesService } from 'app/services/vehicles.service';
 import { StatusesService } from 'app/services/statuses.service';
+import { MapService } from 'app/services/map.service';
 
 
 @Component({
   selector: 'left-nav',
   templateUrl: './left-nav.component.html',
   styleUrls: ['./left-nav.component.css'],
-  providers: [VehiclesService, StatusesService]
+  providers: [VehiclesService, StatusesService, MapService]
 })
 export class LeftNavComponent implements OnInit {
   vehicles;
   statuses;
   VehiclesService;
-  constructor(VehiclesService: VehiclesService, StatusesService: StatusesService) {
+  mapService;
+  constructor(
+    VehiclesService: VehiclesService,
+    StatusesService: StatusesService,
+    MapService: MapService,
+  ) {
     this.VehiclesService = VehiclesService;
     this.statuses = StatusesService.statuses;
+    this.mapService = MapService;
   }
 
   ngOnInit() {
-
     this.VehiclesService.getVehicles().then((data) => {
       this.vehicles = data;
-
-      //if data is empty seed vehicles
-      //dev only
-      if (data.length === 0)
-        this.VehiclesService.seedVehicles();
     });
   }
+
+  filter_by_status(status_id) {
+    console.log('showing cases with status ' + status_id);
+  }
+
+
 
 }
