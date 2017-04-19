d2fd1d93ad88369acb4310e7f620f04a9db18b07
Niczem <nic@transparency-everywhere.com>
2017-04-19 19:01:13 +0200

    updateConfig function added

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 declare var app_config: any;
+declare var fs: any;
 
 @Injectable()
 export class ConfigService {
@@ -9,6 +10,34 @@ export class ConfigService {
   private result: Object;
 
   getConfiguration(key) {
+
+
     return app_config[key]
+  }
+
+  updateConfiguration(key, value, cb) {
+
+    var app_config_string = "var app_config = {\n";
+
+    for (var i in app_config) {
+      if (key == i)
+        app_config_string += '"' + i + '":"' + value + '",\n';
+      else
+        app_config_string += '"' + i + '":"' + app_config[i] + '",\n';
+    }
+
+    //remove last comma
+    app_config_string = app_config_string.slice(0, -1);
+
+    app_config_string += "\n}\n";
+
+    fs.writeFile("src/config/confix.js", app_config_string, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The config was updated.");
+      cb();
+    });
+
   }
 }
