e3e95731f32b64cb73946897b4d830e771411ece
Niczem <nic@transparency-everywhere.com>
2017-05-03 21:51:48 +0200

    cache is cleaned on startup

--- src/index.js
+++ src/index.js
@@ -8,21 +8,27 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1024, height: 768})
 
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`)
+  //clear sesssion on startup 
+  win.webContents.session.clearCache(function(){
+  
+    // and load the index.html of the app.
+    win.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+    // Open the DevTools.
+    win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+    })
+
+  });
+  
 }
 
 // This method will be called when Electron has finished
